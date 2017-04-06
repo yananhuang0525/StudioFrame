1c9b702548de2d2028f7757f772f4d6a96c7ed5c
hyn0525 <hyn_0525@sina.com>
2017-04-06 17:33:22 +0800

    jj

--- app/src/main/java/com/hyn/studioframe/activity/MainActivity.java
+++ app/src/main/java/com/hyn/studioframe/activity/MainActivity.java
@@ -1,9 +1,6 @@
 package com.hyn.studioframe.activity;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
